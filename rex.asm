28213cd13a22edc5290fa7e54e218f1392d98239
Maciej Malecki <malecki.maciej@gmail.com>
2020-02-12 22:55:54 +0100

    further work

--- rex.asm
+++ rex.asm
@@ -5,9 +5,18 @@
 #import "chipset/lib/vic2.asm"
 #import "chipset/lib/cia.asm"
 #import "text/lib/text.asm"
+#import "text/lib/tiles-2x2.asm"
 #import "copper64/lib/copper64.asm"
 
 .filenamespace c64lib
+
+// ZERO page
+.label z_x = 2          // $02,$03
+.label z_y = 4          // $04,$05
+.label z_width = 6      // $06
+.label z_height = 7     // $07
+.label z_map = 8        // $08, $09
+.label z_phase = 10     // $0A
 
 .label VIC_BANK = 3
 .label SCREEN_PAGE_0 = 0
@@ -18,6 +27,8 @@
 .label PLAYER_COL = DARK_GREY
 .label PLAYER_X = 80
 .label PLAYER_Y = 180
+
+.label TILES_COUNT = 4
 
 /*
  * VIC memory layout (16kb):
@@ -44,6 +55,7 @@ start:
   jsr prepareScreen
   jsr configureVic2
   jsr unpackData
+  jsr initBackground
   jsr showPlayer
   
 endless:
@@ -116,10 +128,88 @@ unpackData: {
 
  #import "common/lib/sub/copy-large-mem-forward.asm"
  #import "common/lib/sub/fill-screen.asm"
+ 
+// ------------------- Background ----------------------
 
+//.align $100
+tileColors:
+  .fill 256, DARK_GREY
+mapOffsetsLo:
+  .fill 256, 0
+mapOffsetsHi:
+  .fill 256, 0
+tileDefinition0:
+  .byte $45,$45,$45,$40; .fill 256 - TILES_COUNT, 0
+tileDefinition1:
+  .byte $45,$45,$45,$41; .fill 256 - TILES_COUNT, 0
+tileDefinition2:
+  .byte $45,$43,$44,$42; .fill 256 - TILES_COUNT, 0
+tileDefinition3:
+  .byte $45,$43,$43,$43; .fill 256 - TILES_COUNT, 0
+
+// scrollable background configuration
+.var tilesCfg = Tile2Config()
+.eval tilesCfg.bank = VIC_BANK
+.eval tilesCfg.page0 = SCREEN_PAGE_0
+.eval tilesCfg.page1 = SCREEN_PAGE_1
+.eval tilesCfg.startRow = 1
+.eval tilesCfg.endRow = 24
+.eval tilesCfg.x = z_x
+.eval tilesCfg.y = z_y
+.eval tilesCfg.width = z_width
+.eval tilesCfg.height = z_height
+.eval tilesCfg.tileColors = tileColors
+.eval tilesCfg.mapOffsetsLo = mapOffsetsLo
+.eval tilesCfg.mapOffsetsHi = mapOffsetsHi
+.eval tilesCfg.mapDefinitionPtr = z_map
+.eval tilesCfg.tileDefinition = tileDefinition0
+
+initBackground: {
+  // set map definition pointer
+  lda #<mapDefinition
+  sta z_map
+  lda #>mapDefinition
+  sta z_map + 1
+  // set map dimensions
+  lda #40
+  sta z_width
+  lda #12
+  sta z_height
+  // set [x,y] = [0,0]
+  lda #0
+  sta z_x
+  sta z_x + 1
+  sta z_y
+  sta z_y + 1
+  // set phase to 0
+  lda #0
+  sta z_phase
+  // initialize tile2 system
+  tile2Init(tilesCfg)
+  rts
+}
+
+scrollBackground: {
+  // TODO screen shifting in phases
+  rts
+}
+
+// -- map definition --
+mapDefinition: // 40 x 12
+  .fill 40, 0 // 01
+  .fill 40, 0 // 02
+  .fill 40, 0 // 03
+  .fill 40, 0 // 04
+  .fill 40, 0 // 05
+  .fill 40, 0 // 06
+  .fill 40, 0 // 07
+  .fill 40, 0 // 08
+  .fill 40, 0 // 09
+  .byte 1,1,2,1,1,2,3,1,1,2,1,3,1,3,1,2,3,1,2,2,1,3,3,1,3,1,2,1,3,1,1,2,2,1,2,3,3,3,2,1
+  .fill 40, 0 // 11
+  .fill 40, 0 // 12
 
 // ------------------- DATA ---------------------------
-
 
 // -- Sprites definition --
 beginOfSprites:
@@ -183,30 +273,30 @@ afterOfChargen:
 
   // $40 64
   ch("...##...")//1
-  ch("..#..#..")//2
-  ch(".##..##.")//3
-  ch("..#..#..")//4
-  ch(".##..###")//5
-  ch("..#.....")//6
-  ch(".##..###")//7
-  ch("..#..#..")//8
+  ch("..####..")//2
+  ch(".######.")//3
+  ch("..####..")//4
+  ch(".#######")//5
+  ch("..######")//6
+  ch(".#######")//7
+  ch("..####..")//8
   // $41 65
   ch("........")//1
   ch("..#.....")//2
-  ch(".#.#....")//3
-  ch(".#.##...")//4
-  ch("#..#....")//5
-  ch("..#.....")//6
+  ch(".###....")//3
+  ch(".####...")//4
+  ch("####....")//5
+  ch("###.....")//6
   ch("##......")//7
   ch("........")//8
   // $42 66
-  ch(".##..##.")//1
-  ch("..#..#..")//2
-  ch(".##..##.")//3
-  ch("..#..#..")//4
-  ch("###..###")//5
-  ch("..#..#..")//6
-  ch("..#..#..")//7
+  ch(".######.")//1
+  ch("..####..")//2
+  ch(".######.")//3
+  ch("..####..")//4
+  ch("########")//5
+  ch("..####..")//6
+  ch("..####..")//7
   ch("........")//8
   // $43 67
   ch("........")//1
