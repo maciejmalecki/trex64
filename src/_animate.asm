4cde3366d5a480c54b55dfaa68ad1795cdd3c6d2
Maciej Malecki <malecki.maciej@gmail.com>
2020-05-03 14:27:00 +0200

    more work on anim

--- src/_animate.asm
+++ src/_animate.asm
@@ -2,6 +2,6 @@
 
 .filenamespace c64lib
 
-.label ANIM_CTRL_ENABLE = $01
-.label ANIM_CTRL_REPEAT = $02
-.label ANIM_CTRL_LOOP = $04
+.label ANI_CTRL_ENABLE = $01
+.label ANI_CTRL_REPEAT = $02
+.label ANI_CTRL_LOOP = $04
--- src/animate.asm
+++ src/animate.asm
@@ -4,13 +4,13 @@
 .filenamespace c64lib
 
 .struct AniConfig  {
-    page0,
-    page1,
-    control,
-    sequenceLo,
-    sequenceHI,
-    frames,
-    speedCounters
+  page0,
+  page1,
+  control,
+  sequenceLo,
+  sequenceHI,
+  frames,
+  speedCounters
 }
 
 /*
@@ -21,40 +21,58 @@
  * Mod: A
  */
 .macro ani_setAnimation(aniConfig) {
-    // store control
-    sta aniConfig.control,x
-    // init frame
-    and #ANIM_CTRL_LOOP
-    beq noLoop
-        lda #01
-        jmp initFrame
-    noLoop:
-        lda #00
-    initFrame:
-        sta aniConfig.frames,x
-    // init animation counters
-    lda aniConfig.control,x
-    lsr 
-    lsr
-    lsr
-    lsr
-    sta aniConfig.speedCounters,x
-    // store sequence addresses
-    invokeStackBegin(ptr)
-    pullParamB(sequenceHi)
-    pullParamB(sequenceLo)
-    lda sequenceLo:#$00
-    sta aniConfig.sequenceLo,x
-    lda sequenceHi:#$00
-    sta aniConfig.sequenceHi,x
-    invokeStackEnd(ptr)
-    rts
-    ptr: .word $0000
+  // store control
+  sta aniConfig.control,x
+  // init frame
+  lda #00
+  sta aniConfig.frames,x
+  // init animation counters
+  _ani_calculateSpeedCounterInit(aniConfig)
+  // store sequence addresses
+  invokeStackBegin(ptr)
+  pullParamB(sequenceHi)
+  pullParamB(sequenceLo)
+  lda sequenceLo:#$00
+  sta aniConfig.sequenceLo,x
+  lda sequenceHi:#$00
+  sta aniConfig.sequenceHi,x
+  invokeStackEnd(ptr)
+  rts
+  ptr: .word $0000
+}
+
+/*
+ * In: X - slot number
+ */
+.macro _ani_calculateSpeedCounterInit(aniConfig) {
+  lda aniConfig.control,x
+  lsr
+  lsr
+  lsr
+  lsr
+  sta aniConfig.speedCounters,x
 }
 
 /*
  * Mod: A, X
  */
 .macro ani_animate(aniConfig) {
-
+  ldx #07
+  loopThruSlots:
+    // skip slot if disabled
+    lda aniConfig.control,x
+    and #ANI_CTRL_ENABLE
+    beq nextSlot
+      // decrement speed counter
+      ldy aniConfig.speedCounters,x
+      dey
+      beq speedCounterFinished
+        sty aniConfig.speedCounters,x
+        jmp nextSlot
+      speedCounterFinished:
+        _ani_calculateSpeedCounterInit(aniConfig)
+      // calculate next frame
+    nextSlot:
+    dex
+  bne loopThruSlots
 }
