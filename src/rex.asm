a2c03db89b1078f96327e91d87341f97c2f7da8e
Maciej Malecki <malecki.maciej@gmail.com>
2021-06-24 21:35:44 +0200

    licence

--- src/rex.asm
+++ src/rex.asm
@@ -1,7 +1,7 @@
 /*
   MIT License
   
-  Copyright (c) 2020-2021 Maciej Malecki
+  Copyright (c) 2021 Maciej Malecki
   
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
