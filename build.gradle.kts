6bb0f2a9b240e18aba4aaadfafc713f2731b2c04
Maciej Malecki <malecki.maciej@gmail.com>
2021-08-20 23:28:11 +0200

    plugin 1.4.0 and ctm v8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.c64lib.retroassembler.domain.AssemblerType
 
 plugins {
-    id("com.github.c64lib.retro-assembler") version "1.3.0"
+    id("com.github.c64lib.retro-assembler") version "1.4.0"
 }
 
 repositories {
