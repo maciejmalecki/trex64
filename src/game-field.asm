eae83e687d3177cf40e049a2eb738a226721e6b3
Maciej Malecki <malecki.maciej@gmail.com>
2021-11-17 23:11:47 +0100

    ntsc

--- src/game-field.asm
+++ src/game-field.asm
@@ -176,7 +176,7 @@ _copperListStart:
 // here we define layout of raster interrupt handlers
 ingameCopperList:
     // play music
-    copperEntry(32, IRQH_JSR, <playMusicAndAnimate, >playMusicAndAnimate)
+    copperEntry(40, IRQH_JSR, <playMusicAndAnimate, >playMusicAndAnimate)
     copperEntry(DASHBOARD_Y + 20, IRQH_JSR, <upperMultiplex, >upperMultiplex)
   scrollCode:
     // here we do the actual scrolling
